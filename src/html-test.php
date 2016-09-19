be7ec883b2b5856ed7fa4655b59c0239c71105ac
Alejandro Trujillo J. <contacto@klan1.com>
2016-09-19 13:20:19 -0500

    Some HTML class improvements

--- src/html-test.php
+++ src/html-test.php
@@ -1,9 +1,14 @@
 <?php
+
 include 'init.php';
 
-$html_document = new k1lib\html\html_document_tag("es");
-$html_document->head()->set_title("HTML TEST");
-$html_document->body()->append_p("Hello world");
-$html_document->body()->append_p("Hello world")->append_div("new-div")->append_p("Another P inside the DIV");
+$html = new k1lib\html\html_document_tag("es");
+$html->head()->set_title("HTML TEST");
 
-$html_document->generate_tag(TRUE);
+$html->body()->append_child_tail(new \k1lib\html\script_tag("https://code.jquery.com/jquery-3.1.0.min.js"));
+
+$html->body()->append_p("Hello world");
+
+$html->body()->append_div("new-div")->append_p("An P element inside the DIV element");
+
+$html->generate_tag(TRUE);
