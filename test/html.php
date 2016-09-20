0046480b0bf38a20ef633b5e64c937e007c7d4a1
Alejandro Trujillo J <contacto@klan1.com>
2016-09-20 06:26:30 -0500

    K1 HTML Document implementing

--- test/html.php
+++ test/html.php
@@ -8,13 +8,16 @@ DOM::start();
 
 DOM::html()->head()->set_title("HTML TEST");
 
+$b = DOM::html()->body();
+
+$b->append_child_tail(new \k1lib\html\script_tag("https://code.jquery.com/jquery-3.1.0.min.js"));
+$p = $b->append_p("Hello world","","p1");
+
+$b->append_div("new-div")->append_p("An P element inside the DIV element");
+
 DOM::html()->head()->link_css("https://cdnjs.cloudflare.com/ajax/libs/foundation/6.2.3/foundation.css");
-DOM::html()->body()->append_child_tail(new \k1lib\html\script_tag("https://code.jquery.com/jquery-3.1.0.min.js"));
-DOM::html()->body()->append_child_tail(new \k1lib\html\script_tag("https://cdnjs.cloudflare.com/ajax/libs/foundation/6.2.3/foundation.min.js"));
-DOM::html()->body()->append_child_tail((new \k1lib\html\script_tag())->set_value("$(document).foundation();"));
+$b->append_child_tail(new \k1lib\html\script_tag("https://cdnjs.cloudflare.com/ajax/libs/foundation/6.2.3/foundation.min.js"));
+$b->append_child_tail((new \k1lib\html\script_tag())->set_value("$(document).foundation();"));
 
-DOM::html()->body()->append_p("Hello world");
-
-DOM::html()->body()->append_div("new-div")->append_p("An P element inside the DIV element");
 
 DOM::html()->generate_tag(TRUE);
