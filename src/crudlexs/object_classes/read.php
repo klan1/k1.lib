67d02f42eaee5f6fb4639f36c2f76979da4783eb
Alejandro Trujillo J. <contacto@klan1.com>
2016-10-10 10:28:07 -0500

    Improvements and fixes

--- src/crudlexs/object_classes/read.php
+++ src/crudlexs/object_classes/read.php
@@ -66,8 +66,6 @@ class reading extends crudlexs_base_with_data implements crudlexs_base_interface
             }
             $text_fields_div->append_to($data_group);
 
-//            $div_rows->set_attrib("class", 'end', TRUE);
-
             return $this->div_container;
         } else {
             return FALSE;
