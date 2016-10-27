864b5005019808521475dee61e095082a3b62b22
Alejandro Trujillo J <contacto@klan1.com>
2016-10-27 10:20:04 -0500

    Clean for 0.8 merge

--- src/error_handler/error-handler.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * TODO: Make this... 
- */
-function k1_deprecated($func_name) {
-    trigger_error("Function '" . $func_name . "' do not exist more, please use the new class instead", E_USER_ERROR);
-}
