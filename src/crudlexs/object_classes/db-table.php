7a28737c4ab807558db362e15c18694730a06073
Alejandro Trujillo J <contacto@klan1.com>
2016-11-01 07:21:32 -0500

    - Clientes y proveedores

--- src/crudlexs/object_classes/db-table.php
+++ src/crudlexs/object_classes/db-table.php
@@ -386,7 +386,7 @@ class class_db_table {
                 $this->total_rows_filtered_result = count($query_result) - 1;
                 return $query_result;
             } else {
-                return FALSE;
+                return [];
             }
         } else {
             return FALSE;
