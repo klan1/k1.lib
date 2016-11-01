4d535831dce9e8546863476bc21a398333481a82
Alejandro Trujillo J <contacto@klan1.com>
2016-11-01 15:55:52 -0500

    - Improvements

--- src/crudlexs/object_classes/create.php
+++ src/crudlexs/object_classes/create.php
@@ -498,4 +498,8 @@ class creating extends crudlexs_base_with_data implements crudlexs_base_interfac
         $this->html_form_column_classes = $html_form_column_classes;
     }
 
+    public function get_post_incoming_array() {
+        return $this->post_incoming_array;
+    }
+
 }
