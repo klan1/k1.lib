5012d26a89c6781303e70aa51d68b46306272235
Alejandro Trujillo J <contacto@klan1.com>
2016-11-03 10:38:23 -0500

    - Improvements

--- src/crudlexs/board_classes/create.php
+++ src/crudlexs/board_classes/create.php
@@ -38,6 +38,7 @@ class board_create extends board_base implements board_interface {
             $this->create_object->set_do_table_field_name_encrypt(TRUE);
             $this->controller_object->db_table->set_db_table_show_rule($this->show_rule_to_apply);
             $this->data_loaded = $this->create_object->load_db_table_data(TRUE);
+            $this->create_object->catch_post_data();
             return $this->board_content_div;
         } else {
             DOM_notification::queue_mesasage(board_base_labels::$error_mysql_table_not_opened, "alert", $this->notifications_div_id);
@@ -55,7 +56,7 @@ class board_create extends board_base implements board_interface {
         }
 
         if ($this->data_loaded) {
-            if ($this->create_object->catch_post_data(TRUE)) {
+            if ($this->create_object->get_post_data_catched()) {
                 $this->create_object->put_post_data_on_table_data();
                 if (!$this->skip_form_action) {
                     if ($this->create_object->do_post_data_validation()) {
