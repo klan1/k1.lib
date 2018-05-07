3f42bd11f2693fe78a803ae132fc112f1b35cab6
j0hnd03 <j0hnd03@10.10.22.82>
2018-05-07 02:38:15 -0500

    Now FK fields could have another name than original

--- src/sql/functions.php
+++ src/sql/functions.php
@@ -449,7 +449,7 @@ function sql_insert(\PDO $db, $table, $data, &$error_data = null) {
                 $data_string = array_to_sql_values($data);
                 $insert_sql = "INSERT INTO $table $data_string;";
             }
-            d($insert_sql);
+//            d($insert_sql);
             $insert = $db->exec($insert_sql);
 
             if (isset($db->errorInfo()[2]) && !empty($db->errorInfo()[2])) {
