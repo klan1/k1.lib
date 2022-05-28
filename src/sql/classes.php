39dee3781093616a7a1763aada4c597158eb90e9
j0hnd03 <j0hnd03@localhost>
2022-05-28 10:52:30 -0500

    memcached implemtataion

--- src/sql/classes.php
+++ src/sql/classes.php
@@ -28,6 +28,7 @@ class common_code {
     static public function enable() {
         self::$enabled = TRUE;
     }
+
     /**
      * Disable the engenie
      */
@@ -120,7 +121,24 @@ class profiler extends common_code {
 }
 
 class local_cache extends common_code {
+
 //    use common_code;
+    static protected bool $use_memcached = false;
+
+    /**
+     * 
+     * @var \Memcached
+     */
+    static object $memcached;
+    static protected string $memcached_server = '127.0.0.1';
+    static protected int $memcached_port = 11211;
+    static protected int $memcached_ttl = 300;
+    static protected array $exclude_sql_terms = ['INFORMATION_SCHEMA', 'SHOW'];
+
+    static protected function connect_memcached() {
+        self::$memcached = new \Memcached();
+        self::$memcached->addServer(self::$memcached_server, self::$memcached_port);
+    }
 
     /**
      * Put a SQL_RESULT on the LOCAL CACHE
@@ -128,10 +146,17 @@ class local_cache extends common_code {
      * @param type $sql_result
      */
     static public function add($sql_query, $sql_result) {
-        self::is_enabled(true);
-        $sql_md5 = md5($sql_query);
+        if (self::$use_memcached) {
+            if (!self::check_exlusion($sql_query)) {
+                return self::$memcached->set(md5($sql_query), $sql_result, self::$memcached_ttl);
+            } else {
+                return FALSE;
+            }
+        } else {
+            self::is_enabled(true);
+            self::$data[md5($sql_query)] = $sql_result;
+        }
         self::$data_count++;
-        self::$data[$sql_md5] = $sql_result;
     }
 
     /**
@@ -151,11 +176,70 @@ class local_cache extends common_code {
      */
     static public function get_result($sql_query) {
         self::is_enabled(true);
-        if (isset(self::$data[md5($sql_query)])) {
-            return (self::$data[md5($sql_query)]);
+        if (self::$use_memcached) {
+            if (!self::check_exlusion($sql_query)) {
+                $return = self::$memcached->get(md5($sql_query));
+                return $return;
+            } else {
+                return FALSE;
+            }
         } else {
-            return FALSE;
+            if (isset(self::$data[md5($sql_query)])) {
+                return (self::$data[md5($sql_query)]);
+            } else {
+                return FALSE;
+            }
         }
+    }
+
+    static protected function check_exlusion($sql_query) {
+        foreach (self::$exclude_sql_terms as $term_to_exclude) {
+            if (strstr($sql_query, $term_to_exclude) !== FALSE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static public function get_exclude_sql_terms(): string {
+        return self::$exclude_sql_terms;
+    }
+
+    static public function set_exclude_sql_terms(string $exclude_sql_terms): void {
+        self::$exclude_sql_terms = $exclude_sql_terms;
+    }
+
+    static public function use_memcached() {
+        self::$use_memcached = true;
+        self::connect_memcached();
+    }
+
+    static public function set_mode($mode): void {
+        self::$mode = $mode;
+    }
+
+    static public function get_memcached_server() {
+        return self::$memcached_server;
+    }
+
+    static public function get_memcached_port() {
+        return self::$memcached_port;
+    }
+
+    static public function get_memcached_ttl() {
+        return self::$memcached_ttl;
+    }
+
+    static public function set_memcached_server($memcached_server): void {
+        self::$memcached_server = $memcached_server;
+    }
+
+    static public function set_memcached_port($memcached_port): void {
+        self::$memcached_port = $memcached_port;
+    }
+
+    static public function set_memcached_ttl($memcached_ttl): void {
+        self::$memcached_ttl = $memcached_ttl;
     }
 
 }
