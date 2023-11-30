686d722ff2cb851288e40dde055c6de904feaf03
j0hnd03 <alejo@klan1.com>
2023-11-30 01:58:10 -0500

    new https detector

--- src/urlrewrite/class_url_manager.php
+++ src/urlrewrite/class_url_manager.php
@@ -399,4 +399,9 @@ class url {
             return FALSE;
         }
     }
+
+    static public function is_https() {
+        return
+                (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443;
+    }
 }
