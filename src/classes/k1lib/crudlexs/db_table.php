92b8523094cdc54cf5a7543734467fbc5779686a
j0hnd03 <alejo@klan1.com>
2023-12-11 13:32:29 -0500

    PSR4

--- src/classes/k1lib/crudlexs/db_table.php
+++ src/classes/k1lib/crudlexs/db_table.php
@@ -116,7 +116,7 @@ class db_table {
 
     public function get_db_table_config($reload = FALSE) {
         if ($reload && !empty($this->db_table_config)) {
-            $this->db_table_config = $this->_get_db_table_config($this->db_table_name, TRUE, $reload);
+            $this->db_table_config = $this->_get_db_table_config($this->db_table_name, TRUE, !$reload);
         }
         return $this->db_table_config;
     }
