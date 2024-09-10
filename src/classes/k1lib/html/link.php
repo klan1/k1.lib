19b483052a302e6d7cf1fdd5cbb235b0fa7bf5f3
j0hnd03 <alejo@klan1.com>
2024-09-09 20:09:04 -0500

    v2.1

--- src/classes/k1lib/html/link.php
+++ src/classes/k1lib/html/link.php
@@ -2,20 +2,23 @@
 
 namespace k1lib\html;
 
-class link extends tag {
+class link extends tag
+{
 
     use append_shotcuts;
 
-    function __construct($href) {
+    public function __construct($href, $rel = 'stylesheet', $type = 'text/css')
+    {
         parent::__construct("link");
         if (!empty($href)) {
-            $this->set_attrib("rel", "stylesheet");
+            $this->set_attrib("rel", $rel);
             if (strtolower(substr($href, -4)) == '.css') {
                 $this->set_attrib("type", "text/css");
+            } else {
+                $this->set_attrib("type", $type);
             }
             $this->set_attrib("href", $href);
         }
     }
 
 }
-
