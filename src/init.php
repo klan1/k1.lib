13f124d27e4ce6dc89072c5870888ade04204387
j0hnd03 <alejo@klan1.com>
2026-04-01 18:59:40 -0500

    composer require gettext/translator

--- src/init.php
+++ src/init.php
@@ -22,44 +22,32 @@
 
 namespace k1lib;
 
-use k1lib\lang\translator;
+use k1lib\lang\t;
 
 const IN_K1LIB = TRUE;
 const VERSION = "2.6";
-
-// Peace for user, info for the developer with ZendZerver and Z-Ray Live!
-error_reporting(E_ALL & ~E_NOTICE & ~E_USER_NOTICE);
-
 /*
  * PATH AUTO CONFIG
  */
 define("k1lib\K1LIB_BASE_PATH", dirname(__FILE__));
+/**
+ * This MUST to be reflected on the .htaccess, IF NOT rewriting WONT work!
+ */
+const URL_REWRITE_VAR_NAME = "K1LIB_URL";
+if (!defined("k1lib\K1LIB_LOCALE")) {
+    define("k1lib\K1LIB_LOCALE", "es_CO");
+}
+
+// Peace for user, info for the developer with ZendZerver and Z-Ray Live!
+error_reporting(E_ALL & ~E_NOTICE & ~E_USER_NOTICE);
+
 
 header("PHP-Framework: K1.lib V" . VERSION);
 header("Developed-by: j0hnd03 | http://www.github.com/j0hnd03");
 
 /**
- * This MUST to be reflected on the .htaccess, IF NOT rewriting WONT work!
+ * internationalization 
  */
-const URL_REWRITE_VAR_NAME = "K1LIB_URL";
 
-$translator = translator::getInstance();
-
-//k1lib_include_files(K1LIB_BASE_PATH);
-
-namespace k1lib\html;
-
-const IS_SELF_CLOSED = TRUE;
-const IS_NOT_SELF_CLOSED = FALSE;
-const NO_CLASS = NULL;
-const NO_ID = NULL;
-const NO_VALUE = NULL;
-const APPEND_ON_HEAD = 1;
-const APPEND_ON_MAIN = 2;
-const APPEND_ON_TAIL = 3;
-const INSERT_ON_PRE_TAG = -1;
-const INSERT_ON_AFTER_TAG_OPEN = 2;
-const INSERT_ON_VALUE = 0;
-const INSERT_ON_BEFORE_TAG_CLOSE = 3;
-const INSERT_ON_POST_TAG = 1;
+$translator = t::getInstance();
 
