b26e840c831fd7751bac21ed3d2b6fc85d3b3fb0
j0hnd03 <alejo@klan1.com>
2026-04-21 12:41:20 -0500

    locales before AI

--- locales/en_US/k1lib.php
+++ locales/en_US/k1lib.php
@@ -5,17 +5,18 @@ return [
     'plural-forms' => 'nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);',
     'messages' => [
         '' => [
-            '%ss must be unique for %ss %ss.' => '%ss mora da bude jedinstven za %ss %ss.',
-            'and' => 'i',
-            'This field cannot be null.' => 'Ovo polje ne može ostati prazno.',
-            'This field cannot be blank.' => 'Ovo polje ne može biti prazno.',
-            'Field of type: %ss' => 'Polje tipa: %ss',
-            'Integer' => 'Cijeo broj',
-            '{test1}' => 'test1 <div> test2 </div> test3',
-            '{test2}' => 'test1 <div> test2 </div> test3',
+            'error' => 'Error',
+            'error_hacker' => 'Error! There is a hacker near ?',
+            'alert' => 'Alert',
+            'info' => 'Information',
+            'message' => 'Menssage',
+            'button_submit' => 'Insert',
+            'button_cancel' => 'Cancel',
+            'yes' => 'Yes',
+            'no' => 'No',
+            'fk_tool_name' => 'Select the Foreing Key',
         ],
-        'other-context' =>
-        [
+        'other-context' => [
             'Multibyte test' => '日本人は日本で話される言語です！',
             'Tabulation test' => 'FIELD	FIELD',
         ],
--- locales/es_CO/k1lib.php
+++ locales/es_CO/k1lib.php
@@ -5,17 +5,22 @@ return [
     'plural-forms' => 'nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);',
     'messages' => [
         '' => [
-            'Welcome' => 'Bienvenido',
-            'and' => 'i',
-            'This field cannot be null.' => 'Ovo polje ne može ostati prazno.',
-            'This field cannot be blank.' => 'Ovo polje ne može biti prazno.',
-            'Field of type: %ss' => 'Polje tipa: %ss',
-            'Integer' => 'Cijeo broj',
-            '{test1}' => 'test1 <div> test2 </div> test3',
-            '{test2}' => 'test1 <div> test2 </div> test3',
+            'error' => 'Error',
+            'error_hacker' => 'Error! Is it a hacker near ?',
+            'alert' => 'Alerta',
+            'info' => 'Informacion',
+            'message' => 'Mensaje',
+            'button_submit' => 'Crear',
+            'button_cancel' => 'Cancelar',
+            'yes' => 'Si',
+            'no' => 'No',
+            'fk_tool_name' => 'Seleccione la llave foranea',
         ],
-        'crudlexs' =>
-        [
+        'other-context' => [
+            'Multibyte test' => '日本人は日本で話される言語です！',
+            'Tabulation test' => 'FIELD	FIELD',
+        ],
+        'crudlexs' => [
             'Welcome 1' => 'Bienvenido 1',
             'Tabulation test' => 'FIELD	FIELD',
         ],
